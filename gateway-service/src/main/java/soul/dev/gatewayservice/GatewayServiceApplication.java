64773fbea6cfe823193bacd88feebea987b8e76d
soulaiman ghailan <soulaiman8ghailan@gmail.com>
2024-10-22 21:39:09 +0100

    init api gateway proxy

--- gateway-service/src/main/java/soul/dev/gatewayservice/GatewayServiceApplication.java
+++ gateway-service/src/main/java/soul/dev/gatewayservice/GatewayServiceApplication.java
@@ -2,6 +2,9 @@ package soul.dev.gatewayservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class GatewayServiceApplication {
@@ -10,4 +13,13 @@ public class GatewayServiceApplication {
         SpringApplication.run(GatewayServiceApplication.class, args);
     }
 
+    @Bean
+    RouteLocator gatewayRoutes(RouteLocatorBuilder builder){
+        return builder.routes()
+                .route(r->r.path("/api/v1/researchers/**").uri("http://localhost:8082/"))
+                .route(r->r.path("/api/v1/projects").uri("http://localhost:8081/"))
+                .route(r->r.path("/api/v1/encadrants/**").uri("http://localhost:8083/"))
+                .route(r->r.path("/public/auth/**").uri("http://localhost:8080/"))
+                .build();
+    }
 }
